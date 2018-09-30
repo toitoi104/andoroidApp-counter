e717367cb37512177f5c2270ad27117615986b6c
toitoi104 <flip.tei44@gmail.com>
2018-10-01 02:54:58 +0900

    first commit

---
+++ app/src/main/java/com/seana/counter/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.seana.counter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    var count: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var button = findViewById<Button>(R.id.btn_push)
+        var reset = findViewById<Button>(R.id.btn_reset)
+        var add_counter = findViewById<TextView>(R.id.add_counter)
+
+        //追加
+        button.setOnClickListener {
+            count++
+            var countTxt: String = Integer.toString(count)
+            add_counter.text = countTxt
+        }
+
+        //リセット
+        reset.setOnClickListener {
+            count = 0
+            add_counter.text = "0"
+        }
+    }
+}
